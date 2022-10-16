bfb7b77def33f94d836da48228ac83fdbbe8b0e1
Jean gomes da silva <jean118877@hotmail.com>
2022-10-16 15:27:21 -0300

    em andamento

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Categoria } from "./entities/categoria.entity";
+import { CategoriaService } from "./services/categoria.service";
 
 @Module({
    controllers: [], // Controller
    exports: [TypeOrmModule],
-   imports: [],
-   providers: [], // Service
+   imports: [TypeOrmModule.forFeature([Categoria])],
+   providers: [CategoriaService], // Service
 })
 export class CategoriaModule{}
